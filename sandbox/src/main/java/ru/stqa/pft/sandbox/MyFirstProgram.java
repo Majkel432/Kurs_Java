8e60d8c56b4aa49378fc60300d5861b5f8843212
Majkel432 <46852342+Majkel432@users.noreply.github.com>
2019-01-27 23:55:39 +0100

    Klasa Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,5 +3,20 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        Point p1 = new Point ();
+        p1.x = 2;
+        p1.y = 5;
+        Point p2 = new Point ();
+        p2.x = 5;
+        p2.y = 9;
+    
+        System.out.println(distance(p1,p2));
+    }
+    
+    public static double distance (Point p1, Point p2)
+    {
+        return (Math.sqrt(((p2.x - p1.x)*(p2.x - p1.x))+((p2.y-p2.x)*(p2.y-p2.x))));
+        
     }
 }
