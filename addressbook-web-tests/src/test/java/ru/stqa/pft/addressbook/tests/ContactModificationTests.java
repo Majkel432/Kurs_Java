2ff7cc04f7484d2fab927b5981290778bfd252e8
Majkel432 <46852342+Majkel432@users.noreply.github.com>
2019-02-07 18:24:33 +0100

    DebugContactModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -9,7 +9,7 @@ public class ContactModificationTests extends TestBase
   public void testContactModification()
   {
     app.getContactHelper().InitContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("Mateusz", "Wilk", "example3@poczta.fm", "654321789"));
+    app.getContactHelper().fillContactForm(new ContactData("Jan", "Nowak", "example@poczta.fm", "987654321"));
     app.getContactHelper().SubmitContactModification();
   }
 }
