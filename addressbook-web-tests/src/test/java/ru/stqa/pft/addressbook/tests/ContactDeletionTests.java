1b07dc31e5496b2b68640aa23f3d9ba3e65aefca
Majkel432 <46852342+Majkel432@users.noreply.github.com>
2019-02-19 23:14:46 +0100

    CompareContactsAndGroupsLists

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -19,7 +19,9 @@ public class ContactDeletionTests extends TestBase
     app.getContactHelper().DeleteContact(before.size() - 1);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() - 1);
-    
+  
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
   
 }
