b07818a2bfb2286347fc5b4b367ddc9131273dff
Majkel432 <46852342+Majkel432@users.noreply.github.com>
2019-03-15 17:12:10 +0100

    LambdaGroupsAndContacts

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactTests.java
@@ -3,8 +3,6 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-import ru.stqa.pft.addressbook.model.GroupData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,7 +13,7 @@ public class AddContactTests extends TestBase {
   public void AddContactTest() throws Exception {
     
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("Jan", "Nowak", "example@poczta.fm", "987654321", "test1");
+    ContactData contact = new ContactData("Krzysztof", "Nowak", "example@poczta.fm", "987654321", "test1");
     app.getContactHelper().initContactCreation();
     app.getContactHelper().fillContactForm(contact, true);
     app.getContactHelper().submitContactCreation();
@@ -23,15 +21,8 @@ public class AddContactTests extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
   
-    int max = 0;
-    for (ContactData c : after)
-    {
-      if(c.getId() > max)
-      {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+   
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add (contact);
     Assert.assertEquals(new HashSet<Object>(before),new HashSet<>(after));
     
